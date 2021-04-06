1af10e5f30147bcb1986af1c758d01ab2cbc9a43
Firas Naber <60119746+naber024@users.noreply.github.com>
2021-04-06 10:06:54 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'us-east-1_jzDfAo7Yi',
         userPoolClientId: '3e89bocfrv0cmp0j1i2d9k951b',
-        region: 'us-west-2' // e.g. us-east-2
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
